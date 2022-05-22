b894e0d3087a6836eef58620427ce82dd910ab12
grigorelul <grigorie2002@yahoo.com>
2022-05-22 21:34:29 +0300

    first commit grig

---
+++ android/app/src/main/kotlin/com/cuchi/lacazan/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.cuchi.lacazan
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/ecrane/afisare.dart
@@ -0,0 +1,16 @@
+/// Aici scrie Grig
+
+import 'package:flutter/material.dart';
+
+class Afisare extends StatefulWidget {
+  @override
+  _AfisareState createState() => _AfisareState();
+}
+
+class _AfisareState extends State<Afisare> {
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+    );
+  }
+}
